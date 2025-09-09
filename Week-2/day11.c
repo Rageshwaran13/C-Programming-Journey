0f23224f1457e6f5a489f1d38ce729e4e981c6da
Rageshwaran P <rageshwaran13@gmail.com>
2025-09-09 19:25:08 +0530

    Program to calculate the factorial of a given number using a for loop.
    The program takes an integer n as input.
    
    It initializes a variable fact = 1 because multiplying by 1 keeps the value unchanged.
    
    A for loop runs from 1 to n, multiplying each value with fact.
    
    After the loop ends, the final value of fact is the factorial of the number.
    
    The program then prints the factorial result.

---
+++ Week-2/day11.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int i,n;
+    long long fact=1;
+    printf("Enter the Number:");
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
+    {
+        fact=fact*i;
+    }
+    printf("The factorial of %d is = %lld\n",n,fact);
+}
